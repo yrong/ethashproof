9ab3012d08285ba636cacdcf5fbf6e4eb38904b9
ron <yrong1997@gmail.com>
2023-11-21 13:51:36 +0800

    Update packages

--- ethash/sealer_test.go
+++ ethash/sealer_test.go
@@ -27,8 +27,6 @@ import (
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
-	"github.com/ethereum/go-ethereum/internal/testlog"
-	"github.com/ethereum/go-ethereum/log"
 )
 
 // Tests whether remote HTTP servers are correctly notified of new work.
@@ -71,51 +69,6 @@ func TestRemoteNotify(t *testing.T) {
 		}
 	case <-time.After(3 * time.Second):
 		t.Fatalf("notification timed out")
-	}
-}
-
-// Tests that pushing work packages fast to the miner doesn't cause any data race
-// issues in the notifications.
-func TestRemoteMultiNotify(t *testing.T) {
-	// Start a simple web server to capture notifications.
-	sink := make(chan [3]string, 64)
-	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		blob, err := ioutil.ReadAll(req.Body)
-		if err != nil {
-			t.Errorf("failed to read miner notification: %v", err)
-		}
-		var work [3]string
-		if err := json.Unmarshal(blob, &work); err != nil {
-			t.Errorf("failed to unmarshal miner notification: %v", err)
-		}
-		sink <- work
-	}))
-	defer server.Close()
-
-	// Create the custom ethash engine.
-	ethash := NewTester([]string{server.URL}, false)
-	ethash.config.Log = testlog.Logger(t, log.LvlWarn)
-	defer ethash.Close()
-
-	// Provide a results reader.
-	// Otherwise the unread results will be logged asynchronously
-	// and this can happen after the test is finished, causing a panic.
-	results := make(chan *types.Block, cap(sink))
-
-	// Stream a lot of work task and ensure all the notifications bubble out.
-	for i := 0; i < cap(sink); i++ {
-		header := &types.Header{Number: big.NewInt(int64(i)), Difficulty: big.NewInt(100)}
-		block := types.NewBlockWithHeader(header)
-		ethash.Seal(nil, block, results, nil)
-	}
-
-	for i := 0; i < cap(sink); i++ {
-		select {
-		case <-sink:
-			<-results
-		case <-time.After(10 * time.Second):
-			t.Fatalf("notification %d timed out", i)
-		}
 	}
 }
 
